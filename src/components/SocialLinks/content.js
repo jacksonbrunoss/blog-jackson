2c53364026863e871abe29c8e3e9abd8ad0ac787
jacksonbrunoss <oi.jbss@gmail.com>
2020-03-07 23:12:11 -0300

    SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,28 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/jacksonbrunoss",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/Jack_Bruno25",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/oi.jacksonbss",
+  },
+  {
+    label: "Telegram",
+    url: "https://web.telegram.org/",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/jackson-bruno-105132b3/",
+  },
+  {
+    label: "Discord",
+    url: "https://discordapp.com/login",
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,17 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { Telegram } from "@styled-icons/boxicons-logos/Telegram"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Discord } from "@styled-icons/boxicons-logos/Discord"
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram,
+  Telegram,
+  Linkedin,
+  Discord,
+}
+
+export default Icons
